44b306af42d669d6f194475e0e0d5e1abc5223ec
Pedro Oliveira <pedro_oliva@outlook.com.br>
2026-09-10 10:02:58 -0300

    feat: add pagination schema

--- src/schemas/donor.schema.ts
+++ src/schemas/donor.schema.ts
@@ -1,4 +1,4 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const createDonorSchema = z.object({
   name: z.string().min(3).max(50),
@@ -8,4 +8,9 @@ export const createDonorSchema = z.object({
 export const updateDonorSchema = z.object({
   name: z.string().min(3).max(50).optional(),
   email: z.string().email().max(50).optional(),
-})
+});
+
+export const paginationSchema = z.object({
+  page: z.coerce.number().int().min(1).default(1),
+  limit: z.coerce.number().int().min(1).max(100).default(10),
+});
